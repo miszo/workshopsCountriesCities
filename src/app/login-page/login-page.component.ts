b5ab7fc056202a4c798a1ea500a5368a8a4cdb4e
Miszo Radomski <miszo.radomski@gmail.com>
2017-10-24 19:01:45 +0200

    package-lock and component folder

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public player = {
+    name: ''
+  };
+
+  public singIn(playerName) {
+    console.log(playerName);
+  }
+}
